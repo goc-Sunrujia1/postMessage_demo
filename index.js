b2dd48b3e855418db4c3da2b3c8e9e7ebd73844b
sunrujia <sunrujia-1@gocyber.world>
2022-06-24 06:55:46 +0800

    demo

---
+++ index.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var fs = require('fs');
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) throw err;    
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(3001);
+});
+
+fs.readFile('./iframe.html', function (err, html) {
+    if (err) throw err;    
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(3000);
+});
+
+
